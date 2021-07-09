e45f5da1c61d933bd8786a7c7ceecbccaebbbf6f
Neil Smith <s3nt1n3lz18@gmail.com>
2021-07-09 12:39:36 +0100

    renamed helper function

--- src/app/htmlTests/9_testingngSelect/testing9.component.spec.ts
+++ src/app/htmlTests/9_testingngSelect/testing9.component.spec.ts
@@ -26,32 +26,75 @@ describe('Testing NgSelect', () => {
       expect(component).toBeTruthy();
   });
 
-  it('should call the function selectOption when the user selects a new value from the dropdown', () => {
-    component.options = ['option 1','option 2','option 3'];
-    component.option = 'option 1';
-    componentFixture.detectChanges();
-    const spy = spyOn(component, 'selectOption');
-    const element = componentFixture.debugElement.query(By.css('#optionDropdown'));
+  describe('query', () => {
+    it('should call the function selectOption when the user selects a new value from the dropdown', () => {
+      //Assign
+      component.options = ['option 1','option 2','option 3'];
+      component.option = 'option 1';
+      componentFixture.detectChanges();
+      const spy = spyOn(component, 'selectOption');
+      const element = componentFixture.debugElement.query(By.css('#optionDropdown'));
+  
+      //Act
+      triggerKeyDownEvent(element, 32); // space to open the dropdown
+      triggerKeyDownEvent(element, 40); // down arrow
+      triggerKeyDownEvent(element, 13); // enter to select second option and close the dropdown
+      
+      //Assert
+      expect(spy).toHaveBeenCalledWith('option 2');
+    });
+  
+    it('should update the field option when the user selects a new value from the dropdown', () => {
+      //Assign
+      component.options = ['option 1','option 2','option 3'];
+      component.option = 'option 1';
+      componentFixture.detectChanges();
+      const element = componentFixture.debugElement.query(By.css('#optionDropdown'));
+      
+      //Act
+      triggerKeyDownEvent(element, 32); // space to open the dropdown
+      triggerKeyDownEvent(element, 40); // down arrow
+      triggerKeyDownEvent(element, 13); // enter to select second option and close the dropdown
+  
+      //Assert
+      expect(component.option).toEqual('option 2');
+    });
+  })
 
-    triggerKeyDownEvent(element, 32); // space to open the dropdown
-    triggerKeyDownEvent(element, 40); // down arrow
-    triggerKeyDownEvent(element, 13); // enter to select second option and close the dropdown
-    
-    expect(spy).toHaveBeenCalledWith('option 2');
-  });
-
-  it('should update the field option when the user selects a new value from the dropdown', () => {
-    component.options = ['option 1','option 2','option 3'];
-    component.option = 'option 1';
-    componentFixture.detectChanges();
-    const element = componentFixture.debugElement.query(By.css('#optionDropdown'));
-    
-    triggerKeyDownEvent(element, 32); // space to open the dropdown
-    triggerKeyDownEvent(element, 40); // down arrow
-    triggerKeyDownEvent(element, 13); // enter to select second option and close the dropdown
-
-    expect(component.option).toEqual('option 2');
-  });
+  describe('queryAll', () => {
+    it('should call the function selectOption when the user selects a new value from the dropdown', () => {
+      //Assign
+      component.options = ['option 1','option 2','option 3'];
+      component.option = 'option 1';
+      componentFixture.detectChanges();
+      const spy = spyOn(component, 'selectOption');
+      const elements = componentFixture.debugElement.queryAll(By.css('#optionDropdown'));
+  
+      //Act
+      triggerKeyDownEvent(elements[0], 32); // space to open the dropdown
+      triggerKeyDownEvent(elements[0], 40); // down arrow
+      triggerKeyDownEvent(elements[0], 13); // enter to select second option and close the dropdown
+      
+      //Assert
+      expect(spy).toHaveBeenCalledWith('option 2');
+    });
+  
+    it('should update the field option when the user selects a new value from the dropdown', () => {
+      //Assign
+      component.options = ['option 1','option 2','option 3'];
+      component.option = 'option 1';
+      componentFixture.detectChanges();
+      const elements = componentFixture.debugElement.queryAll(By.css('#optionDropdown'));
+      
+      //Act
+      triggerKeyDownEvent(elements[0], 32); // space to open the dropdown
+      triggerKeyDownEvent(elements[0], 40); // down arrow
+      triggerKeyDownEvent(elements[0], 13); // enter to select second option and close the dropdown
+  
+      //Assert
+      expect(component.option).toEqual('option 2');
+    });
+  })
 
   function triggerKeyDownEvent(element: DebugElement, which: number, key = ''): void {
     element.triggerEventHandler('keydown', {
